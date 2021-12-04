e18574b25cc03f0704e52ff8aa1ba21af0cfcb90
NoahLaroi <noahll597@gmail.com>
2021-12-04 18:47:20 -0500

    Bugs

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,30 +1,28 @@
-// let showsArray = [
-//     {date:'Mon Sept 06 2021', venue: 'Ronald Lane', location: 'San Francisco, CA'}, { date: 'Tue Sept 21, 2021', venue: 'Pier 3 East', location: 'San Francisco, CA'}, {date: 'Tue Sept 21 2021', venue:'View Lounge', location: 'San Francisco, CA'}, 
-// ]
-// function createTable(table) {
-//     let thead = table.creatTHead();
-//     let row = thead.insertRow();
-// }
-
-(function() { // < This line is similar to window.onLoad, it just makes sure that everything inside will run
-    const section = [
-        { name: "Ada", description: "medium, skinny, stinky" },
-        { name: "Rider", description: "big, buff, stinky" },
-        { name: "Pasha", description: "small, scrappy, stinky" },
+window.onload = () => {
+    let showsArray = [{ date: 'Mon Sept 06 2021', venue: 'Ronald Lane', location: 'San Francisco, CA' },
+        { date: 'Tue Sept 21, 2021', venue: 'Pier 3 East', location: 'San Francisco, CA' },
+        { date: 'Tue Sept 21 2021', venue: 'View Lounge', location: 'San Francisco, CA' },
+        { date: 'Nov 06 2021', venue: 'Hyatt Agency', location: 'San Francisco, CA' },
+        { date: 'Fri Nov 26 2021', venue: 'Moscow Center', location: 'San Francisco, CA' },
+        { date: 'Wed Dec 15 2021', venue: 'Press Club', location: 'San Francisco, CA' }
     ];
+    for (let i = 0; i < showsArray.length; i++) {
+        console.log(i)
+        let pDate = document.createElement("p")
+        pDate.classList.add("show__info")
+        pDate.innerHTML = showsArray[i].date
 
-    //get section where I would like to add my element to it
-    const namesSection = document.getElementsByClassName("name-list");
+        let pVenue = document.createElement("p")
+        pVenue.classList.add("show__info")
+        pVenue.innerHTML = "venue";
 
-    //Looping through my dogs array
-    for (i = 0; i < comments.length; i++) {
-        //create html to store the name value
-        let p = document.createElement("P");
-        //add a class so I know what to style *Not required
-        p.classList.add("name-tag");
-        //add the value of array to the html element
-        p.innerHTML = comments[i].name;
-        //append html element to the section
-        namesSection[0].appendChild(p);
+        let pLocation = document.createElement("p")
+        pVenue.classList.add("show__info")
+        pVenue.innerHTML = showsArray[i].location
+
+        let mainShow = document.querySelector('.mainShow')
+        mainShow.appendChild(pDate);
+        mainShow.appendChild(pVenue);
+        mainShow.appendChild(pLocation);
     }
-})();
+};
