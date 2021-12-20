bf4ec776c192caf709810528d3a243ecf6738341
NoahLaroi <noahll597@gmail.com>
2021-12-20 07:39:44 -0500

    Final edits!

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,137 +1,151 @@
 window.onload = () => {
-    let showsArray = [{ date: 'Mon Sept 06 2021', venue: 'Ronald Lane', location: 'San Francisco, CA' },
-        { date: 'Tue Sept 21, 2021', venue: 'Pier 3 East', location: 'San Francisco, CA' },
-        { date: 'Tue Sept 21 2021', venue: 'View Lounge', location: 'San Francisco, CA' },
-        { date: 'Nov 06 2021', venue: 'Hyatt Agency', location: 'San Francisco, CA' },
-        { date: 'Fri Nov 26 2021', venue: 'Moscow Center', location: 'San Francisco, CA' },
-        { date: 'Wed Dec 15 2021', venue: 'Press Club', location: 'San Francisco, CA' }
-    ];
+    const keyCode = { api_key: '755ba649-59c6-4b86-9bbd-e96a2b149b2e' };
+    let showsArray = axios
+        .get('https://project-1-api.herokuapp.com/showdates?api_key=y55ba649-59c6-4b86-9bbd-e96a2b149b2e')
+        .then((response) => {
+
+            return (response.data)
+        })
+        .then((response) => {
+
+            dateLoop(response)
+            altDateLoop(response)
+        });
     let mainShow = document.querySelector('.mainShow');
     let mainShowTitle = document.createElement('h2');
     mainShowTitle.classList.add('mainShowTitle');
     mainShowTitle.innerHTML = 'Shows'
     mainShow.appendChild(mainShowTitle);
 
-    for (let i = 0; i < showsArray.length; i++) {
-        console.log(i)
-        let mainShowButton = document.createElement('button')
-        mainShowButton.classList.add('mainShowButton')
-        mainShowButton.innerHTML = 'BUY TICKETS'
 
-        let showBreak = document.createElement('hr')
-        showBreak.classList.add('showBreak');
+    function dateLoop(showsArray) {
+        for (let i = 0; i < showsArray.length; i++) {
 
-        let dateTitle = document.createElement('h3');
-        dateTitle.innerHTML = 'DATE'
+            let mainShowButton = document.createElement('button')
+            mainShowButton.classList.add('mainShowButton')
+            mainShowButton.innerHTML = 'BUY TICKETS'
 
-        let pDate = document.createElement("p")
-        pDate.classList.add("show__info")
-        pDate.innerHTML = showsArray[i].date
+            let showBreak = document.createElement('hr')
+            showBreak.classList.add('showBreak');
 
-        let venueTitle = document.createElement('h3');
-        venueTitle.innerHTML = 'VENUE'
+            let dateTitle = document.createElement('h3');
+            dateTitle.innerHTML = 'DATE'
 
-        let pVenue = document.createElement("p")
-        pVenue.classList.add("show__info")
-        pVenue.innerHTML = showsArray[i].venue
+            let pDate = document.createElement("p")
+            let humanReadableDate = new Date(Number(showsArray[i].date)).toDateString()
+            pDate.classList.add("show__info")
+            pDate.innerHTML = humanReadableDate;
 
-        let locationTitle = document.createElement('h3');
-        locationTitle.innerHTML = 'LOCATION'
+            let venueTitle = document.createElement('h3');
+            venueTitle.innerHTML = 'VENUE'
 
-        let pLocation = document.createElement("p")
-        pLocation.classList.add("show__info")
-        pLocation.innerHTML = showsArray[i].location
+            let pVenue = document.createElement("p")
+            pVenue.classList.add("show__info")
+            pVenue.innerHTML = showsArray[i].place
 
-        let mainShow = document.querySelector('.mainShow')
+            let locationTitle = document.createElement('h3');
+            locationTitle.innerHTML = 'LOCATION'
+
+            let pLocation = document.createElement("p")
+            pLocation.classList.add("show__info")
+            pLocation.innerHTML = showsArray[i].location
+
+            let mainShow = document.querySelector('.mainShow')
 
 
-        mainShow.appendChild(dateTitle);
-        mainShow.appendChild(pDate);
-        mainShow.appendChild(venueTitle);
-        mainShow.appendChild(pVenue);
-        mainShow.appendChild(locationTitle);
-        mainShow.appendChild(pLocation);
-        mainShow.appendChild(mainShowButton);
-        mainShow.appendChild(showBreak);
-    }
-    let altShowsArray = [{ date: 'Mon Sept 06 2021', venue: 'Ronald Lane', location: 'San Francisco, CA' },
-        { date: 'Tue Sept 21, 2021', venue: 'Pier 3 East', location: 'San Francisco, CA' },
-        { date: 'Tue Sept 21 2021', venue: 'View Lounge', location: 'San Francisco, CA' },
-        { date: 'Nov 06 2021', venue: 'Hyatt Agency', location: 'San Francisco, CA' },
-        { date: 'Fri Nov 26 2021', venue: 'Moscow Center', location: 'San Francisco, CA' },
-        { date: 'Wed Dec 15 2021', venue: 'Press Club', location: 'San Francisco, CA' }
-    ];
-    let altMainShow = document.querySelector('.altMainShow');
+            mainShow.appendChild(dateTitle);
+            mainShow.appendChild(pDate);
+            mainShow.appendChild(venueTitle);
+            mainShow.appendChild(pVenue);
+            mainShow.appendChild(locationTitle);
+            mainShow.appendChild(pLocation);
+            mainShow.appendChild(mainShowButton);
+            mainShow.appendChild(showBreak);
+        }
 
-    let altMainShowTitle = document.createElement('h2');
-    altMainShowTitle.classList.add('altMainShowTitle');
-    altMainShowTitle.innerHTML = 'Shows'
-    altMainShow.appendChild(altMainShowTitle);
+    };
 
-    let altMainShowTable = document.createElement('table');
-    altMainShowTable.classList.add('altMainShowTable');
-    altMainShow.appendChild(altMainShowTable);
+    function altDateLoop(altShowsArray) {
+        // let altShowsArray = [{ date: 'Mon Sept 06 2021', venue: 'Ronald Lane', location: 'San Francisco, CA' },
+        //     { date: 'Tue Sept 21, 2021', venue: 'Pier 3 East', location: 'San Francisco, CA' },
+        //     { date: 'Tue Sept 21 2021', venue: 'View Lounge', location: 'San Francisco, CA' },
+        //     { date: 'Nov 06 2021', venue: 'Hyatt Agency', location: 'San Francisco, CA' },
+        //     { date: 'Fri Nov 26 2021', venue: 'Moscow Center', location: 'San Francisco, CA' },
+        //     { date: 'Wed Dec 15 2021', venue: 'Press Club', location: 'San Francisco, CA' }
+        // ];
+        let altMainShow = document.querySelector('.altMainShow');
 
-    let altDateTitle = document.createElement('thead');
-    altDateTitle.innerHTML = 'DATE'
-    altMainShowTable.appendChild(altDateTitle);
+        let altMainShowTitle = document.createElement('h2');
+        altMainShowTitle.classList.add('altMainShowTitle');
+        altMainShowTitle.innerHTML = 'Shows'
+        altMainShow.appendChild(altMainShowTitle);
 
-    let altVenueTitle = document.createElement('thead');
-    altVenueTitle.innerHTML = 'VENUE'
-    altMainShowTable.appendChild(altVenueTitle);
+        let altMainShowTable = document.createElement('table');
+        altMainShowTable.classList.add('altMainShowTable');
+        altMainShow.appendChild(altMainShowTable);
 
-    let altLocationTitle = document.createElement('thead');
-    altLocationTitle.innerHTML = 'LOCATION'
-    altMainShowTable.appendChild(altLocationTitle);
+        let altDateTitle = document.createElement('thead');
+        altDateTitle.innerHTML = 'DATE'
+        altMainShowTable.appendChild(altDateTitle);
 
-    let buttonRowHead = document.createElement('thead');
-    buttonRowHead.classList.add('buttonRowHead');
-    buttonRowHead.innerHTML = 'Some Text'
-    altMainShowTable.appendChild(buttonRowHead)
+        let altVenueTitle = document.createElement('thead');
+        altVenueTitle.innerHTML = 'VENUE'
+        altMainShowTable.appendChild(altVenueTitle);
 
-    let dateRow = document.createElement('tr');
-    dateRow.classList.add("dateRow");
-    altDateTitle.appendChild(dateRow);
+        let altLocationTitle = document.createElement('thead');
+        altLocationTitle.innerHTML = 'LOCATION'
+        altMainShowTable.appendChild(altLocationTitle);
 
-    let venueRow = document.createElement('tr');
-    venueRow.classList.add("venueRow");
-    altVenueTitle.appendChild(venueRow);
+        let buttonRowHead = document.createElement('thead');
+        buttonRowHead.classList.add('buttonRowHead');
+        buttonRowHead.innerHTML = 'Some Text'
+        altMainShowTable.appendChild(buttonRowHead)
 
-    let locationRow = document.createElement('tr');
-    locationRow.classList.add("locationRow");
-    altLocationTitle.appendChild(locationRow);
+        let dateRow = document.createElement('tr');
+        dateRow.classList.add("dateRow");
+        altDateTitle.appendChild(dateRow);
 
-    let buttonRow = document.createElement('tr')
-    buttonRow.classList.add('buttonRow');
-    buttonRowHead.appendChild(buttonRow);
+        let venueRow = document.createElement('tr');
+        venueRow.classList.add("venueRow");
+        altVenueTitle.appendChild(venueRow);
 
-    for (let i = 0; i < altShowsArray.length; i++) {
-        console.log(i)
+        let locationRow = document.createElement('tr');
+        locationRow.classList.add("locationRow");
+        altLocationTitle.appendChild(locationRow);
 
-        let altPDate = document.createElement("td")
-        altPDate.classList.add("altPDate")
-        altPDate.innerHTML = altShowsArray[i].date
+        let buttonRow = document.createElement('tr')
+        buttonRow.classList.add('buttonRow');
+        buttonRowHead.appendChild(buttonRow);
 
-        let altPVenue = document.createElement("td")
-        altPVenue.classList.add("altPVenue")
-        altPVenue.innerHTML = altShowsArray[i].venue
+        for (let i = 0; i < altShowsArray.length; i++) {
 
-        let altPLocation = document.createElement("td")
-        altPLocation.classList.add("altpLocation")
-        altPLocation.innerHTML = altShowsArray[i].location
 
-        let altMainShowButtonContainer = document.createElement('td')
-        altMainShowButtonContainer.classList.add('altMainShowButtonContainer')
+            let altPDate = document.createElement("td")
+            altPDate.classList.add("altPDate")
+            let humanReadableDate = new Date(Number(altShowsArray[i].date)).toDateString()
+            altPDate.innerHTML = humanReadableDate;
 
-        let altMainShowButton = document.createElement("div")
-        altMainShowButton.classList.add('mainShowButton')
-        altMainShowButton.innerText = 'BUY TICKETS'
+            let altPVenue = document.createElement("td")
+            altPVenue.classList.add("altPVenue")
+            altPVenue.innerHTML = altShowsArray[i].place
 
-        dateRow.appendChild(altPDate);
-        venueRow.appendChild(altPVenue);
-        locationRow.appendChild(altPLocation);
-        buttonRow.appendChild(altMainShowButton);
-        buttonRowHead.appendChild(altMainShowButtonContainer);
-        altMainShowButtonContainer.appendChild(altMainShowButton);
+            let altPLocation = document.createElement("td")
+            altPLocation.classList.add("altpLocation")
+            altPLocation.innerHTML = altShowsArray[i].location
+
+            let altMainShowButtonContainer = document.createElement('td')
+            altMainShowButtonContainer.classList.add('altMainShowButtonContainer')
+
+            let altMainShowButton = document.createElement("div")
+            altMainShowButton.classList.add('mainShowButton')
+            altMainShowButton.innerText = 'BUY TICKETS'
+
+            dateRow.appendChild(altPDate);
+            venueRow.appendChild(altPVenue);
+            locationRow.appendChild(altPLocation);
+            buttonRow.appendChild(altMainShowButton);
+            buttonRowHead.appendChild(altMainShowButtonContainer);
+            altMainShowButtonContainer.appendChild(altMainShowButton);
+        }
     }
 };
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,68 +1,97 @@
 window.onload = () => {
-    commentsArray = [{ name: 'Connor Walton', date: '02/17/2021', comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' }, { name: 'Emilie Beach', date: '01/09/2021', comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' }, {
-        name: 'Miles Acosta',
-        date: '12/20/2020',
-        comment: 'I can\'t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can/t get enough.'
-    }];
+    const keyCode = { api_key: '755ba649-59c6-4b86-9bbd-e96a2b149b2e' };
+    let commentsArray = axios
+        .get('https://project-1-api.herokuapp.com/comments?api_key=y55ba649-59c6-4b86-9bbd-e96a2b149b2e')
+        .then((response) => {
 
+            return (response.data)
+        })
+        .then((response) => {
+
+            commentLoop(response)
+        });
     const form = document.getElementById('userForm');
-    const postedComments = document.querySelector('#postedComments')
-    const commentSection = document.querySelector('CommentSection')
+    const postedComments = document.querySelector('#postedComments');
+    const commentSection = document.querySelector('CommentSection');
 
     let commentBoxDiv = document.createElement('div');
     commentBoxDiv.classList.add('commentBox');
     postedComments.append(commentBoxDiv);
-    for (let i = 0; i < commentsArray.length; i++) {
-        let profileImg = document.createElement('div');
-        profileImg.classList.add('profileImg');
-        //This is the code for the names to be implemented 
-        let nameTag = document.createElement('p');
-        nameTag.classList.add('name');
-        nameTag.innerHTML = commentsArray[i].name;
-        //This is the code for the dates to be implemented 
-        let dateTag = document.createElement('p');
-        dateTag.classList.add('date');
-        dateTag.innerHTML = commentsArray[i].date;
-        //This div separates the name and profile picture from the date 
-        let nameAndPic = document.createElement('div')
-        nameAndPic.classList.add('nameAndPic')
-        nameAndPic.appendChild(profileImg)
-        nameAndPic.appendChild(nameTag)
-            //This is a div that I created in order 
-            //to house the profile, name, and date 
-            //so that they span across the top of 
+
+    function commentLoop(commentsArray) {
+        for (let i = 0; i < commentsArray.length; i++) {
+            let profileImg = document.createElement('div');
+            profileImg.classList.add('profileImg');
+            //This is the code for the names to be implemented
+            let nameTag = document.createElement('p');
+            nameTag.classList.add('name');
+            nameTag.innerHTML = commentsArray[i].name;
+            //This is the code for the dates to be implemented -- The UTC date had to be converted so that it is readable
+            let dateTag = document.createElement('p');
+            let humanReadableDate = new Date(Number(commentsArray[i].timestamp)).toLocaleDateString()
+            dateTag.classList.add('timestamp');
+            dateTag.innerHTML = humanReadableDate;
+            //This div separates the name and profile picture from the date
+            let nameAndPic = document.createElement('div');
+            nameAndPic.classList.add('nameAndPic');
+            nameAndPic.appendChild(profileImg);
+            nameAndPic.appendChild(nameTag);
+            //This is a div that I created in order
+            //to house the profile, name, and date
+            //so that they span across the top of
             //the comment box
-        let topContainer = document.createElement('div');
-        topContainer.classList.add('topContainer')
-        commentBoxDiv.appendChild(topContainer);
-        topContainer.appendChild(nameAndPic);
-        topContainer.appendChild(dateTag);
-        //This is the code for the comments to be implemented 
-        let postedCommentText = document.createElement('p');
-        postedCommentText.classList.add('comment');
-        postedCommentText.innerHTML = commentsArray[i].comment;
-        commentBoxDiv.appendChild(postedCommentText);
-        //This is where I will add my divider 
-        let Boxbreak = document.createElement('hr');
-        commentBoxDiv.appendChild(Boxbreak)
-    }
+            let topContainer = document.createElement('div');
+            topContainer.classList.add('topContainer');
+            commentBoxDiv.appendChild(topContainer);
+            topContainer.appendChild(nameAndPic);
+            topContainer.appendChild(dateTag);
+            //This is the code for the comments to be implemented
+            let postedCommentText = document.createElement('p');
+            postedCommentText.classList.add('comment');
+            postedCommentText.innerHTML = commentsArray[i].comment;
+            commentBoxDiv.appendChild(postedCommentText);
+            //This is where I will add my divider
+            let Boxbreak = document.createElement('hr');
+            commentBoxDiv.appendChild(Boxbreak);
+        }
+    };
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
+        //This is all the coding for rendering the current date
         const currentDate = new Date();
         const currentDay = currentDate.getDate();
         const currentMonth = currentDate.getMonth();
         const currentYear = currentDate.getFullYear();
-        const fullDate = (currentMonth + 1) + "/" + currentDay + "/" + currentYear;
-        document.createElement('p');
-        fullDate
-        let profilePicNewComment = document.querySelector('.profilePicture');
-        let userName = event.target.user_name.value
-        let commentText = event.target.comment_submission.value
-        let newComment = profilePicNewComment + userName + fullDate + commentText;
-        let newCommentDiv = document.createElement('div')
-        newCommentDiv.classList.add('commentBox')
-        newCommentDiv.innerHTML = newComment
+        const fullDate = document.createElement('p');
+        fullDate.innerHTML = currentMonth + 1 + '/' + currentDay + '/' + currentYear;
+        let profilePicNewComment = document.createElement('div');
+        profilePicNewComment.classList.add('newProfilePic');
+        let commentBreak = document.createElement('hr');
+        let userName = document.createElement('p');
+        userName.innerHTML = event.target.user_name.value;
+        let commentText = document.createElement('p');
+        commentText.classList.add('comment')
+        commentText.innerHTML = event.target.comment_submission.value;
+        let newCommentDiv = document.createElement('div');
+        let topContainer = document.createElement('div');
+        topContainer.classList.add('topContainer');
+        let nameAndPic = document.createElement('div');
+        // let newComment = userName + fullDate + commentText;
+        nameAndPic.classList.add('nameAndPic')
+        nameAndPic.appendChild(profilePicNewComment);
+        nameAndPic.appendChild(userName);
+        topContainer.appendChild(nameAndPic);
+        topContainer.appendChild(fullDate);
+        //The new comment to be generated at the form submission event
+        newCommentDiv.classList.add('commentBox');
+        // newCommentDiv.innerHTML = newComment;
         postedComments.prepend(newCommentDiv);
+        newCommentDiv.appendChild(topContainer);
+        newCommentDiv.appendChild(commentText)
+        newCommentDiv.appendChild(commentBreak);
+        //Clear comments on submission event
+        document.querySelector("input").value = '';
+        document.querySelector("textarea").value = '';
     });
-}
+};
